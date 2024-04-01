bffb6f68092a27b65a2ef4be567fb5718b9814c7
DanielHansen-IMD <daniel.hansen@confederationcollege.ca>
2024-04-01 09:47:58 -0400

    added fade transition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+     import { fly } from 'svelte/transition';
      import '../style.css';
      import { writable } from 'svelte/store';
      let todoItem = '';
@@ -50,7 +51,7 @@
      </form>
      <ul>
           {#each $todoList as item, index}
-               <li class:done={item.done} >
+               <li transition:fly={{y: 15, duration: 200}} class:done={item.done} >
                     <input type="checkbox" bind:checked={item.done} on:change={updateList}>
                     <span class="text">{item.text}</span>
                     <span on:click={() => removeThis(index)} class="remove" role="button" tabindex="0">&times;</span>
@@ -114,6 +115,7 @@
           font-size: 1.2em;
           color: #f8f8f8;
           background-color:rgb(20, 153, 95);
+          cursor: pointer;
      }
      li {
           font-size: 1.3rem;
